46a02cd1fafbbd5f0b48291d840c064fdd6e6e10
alekniko <aleksejs.nikolajevs@gmail.com>
2019-09-10 11:33:58 +0300

    Add BOOLEAN examples 10.09.2019

---
+++ boolean.py
@@ -0,0 +1,8 @@
+# Python comes with Booleans (with predefined True and False displays that are basically just the integers 1 and 0).
+# It also has a placeholder object called None. Let's walk through a few quick examples of Booleans
+# (we will dive deeper into them later in this course).
+b = None
+print(1 < 2)
+print(1 > 2)
+print(1 == 1)
+print(type(b))
