f453a42eaaab12909d1eff9653ea51d06c1c0d01
alekniko <aleksejs.nikolajevs@gmail.com>
2019-09-10 11:29:34 +0300

    Add SET examples 10.09.2019

---
+++ set.py
@@ -0,0 +1,15 @@
+#   Sets are an unordered collection of unique elements.
+#   We can construct them by using the set() function.
+
+myset = set()
+myset.add(1)
+myset.add(2)
+myset.add(2)
+print(myset)
+
+myList = [7, 1, 1, 3, 1, 2, 1, 6, 4, 5, 5, 4, 3, 2, 1, ]
+print(myList)
+
+print(set(myList))
+s = 'Mississippi'
+print(set(s))
