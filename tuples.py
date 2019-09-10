87de005ecf127a220bb4100da67ce9698e82c6a2
alekniko <aleksejs.nikolajevs@gmail.com>
2019-09-10 11:14:43 +0300

    Extended TUPLES example 10.09.2019

--- tuples.py
+++ tuples.py
@@ -1,4 +1,10 @@
-t = (1,2,3)
+t = (1, 2, 3)
 myList = [1, 2, 3]
 print(type(t))
 print(myList)
+
+t2 = ('a', 'b', 'a', 'c')
+print(t2.count('a'))
+print(t2.index('c'))
+myList[0] = 'New'
+#   t2[0] = 'new' - tuples can't be modified
